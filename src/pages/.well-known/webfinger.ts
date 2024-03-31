49325a9b29b1473fe74f7a5c20ec7bb9218a7a96
Sebastiaan Benjamins <sebastiaan@benjami.in>
2024-03-31 16:00:01 +0200

    Update webfinger.ts

--- src/pages/.well-known/webfinger.ts
+++ src/pages/.well-known/webfinger.ts
@@ -4,19 +4,28 @@ import { APIRoute } from "astro";
 import { accounts, db, eq } from "astro:db";
 
 const createWebfinger = (username: string) => {
+	const endpoint = userEndpoint(username);
+
 	return {
 		subject: `acct:${toFullMention(username)}`,
 
+		aliases: [endpoint],
+
 		links: [
-			{
-				rel: "http://webfinger.net/rel/profile-page",
-				type: "text/html",
-				href: userEndpoint(username),
-			},
 			{
 				rel: "self",
 				type: "application/activity+json",
-				href: userEndpoint(username),
+				href: endpoint,
+			},
+			{
+				rel: "http://webfinger.net/rel/profile-page",
+				type: "text/html",
+				href: endpoint,
+			},
+			{
+				rel: "http://webfinger.net/rel/avatar",
+				type: "image/png",
+				href: "https://media.mas.to/accounts/avatars/109/772/734/566/866/469/original/94d402ddf59e1507.png",
 			},
 		],
 	};
